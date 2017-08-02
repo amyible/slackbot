bec41765d1169c6bfb9ffe102d00298d8b5a915d
David Wolff <davidwolff2020@u.northwestern.edu>
2017-08-02 10:41:16 -0700

    l

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var path = require('path');
 var { router, addAllDayEvents } = require('./routes');
 var models = require('./models/models');
 var User = models.User;
+var Reminder = models.Reminder;
 
 var app = express();
 
@@ -237,7 +238,7 @@ app.post('/interact', function(req, res) {
       var subject = splitted.join(' ');
       console.log('subject: ', subject);
       console.log('day: ', day);
-      
+
       User.find({slack_id: answer.user.id})
       .exec(function(err, user){
           console.log("USER", user);
